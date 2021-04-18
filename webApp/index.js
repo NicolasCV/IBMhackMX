ab57a2b5e73de99255c3abe851ed969d264fbf58
Nicolas CV <niccarval@hotmail.com>
2021-04-18 17:52:26 -0600

    PagWeb
    La primera parte de la pagina web ya tiene elementos de javascript

---
+++ webApp/index.js
@@ -0,0 +1,25 @@
+
+var textArea = document.getElementById("inputT");
+var ticketLabel = document.getElementById("TL");
+var lengthCode = 5;
+
+function enter(){
+    goAhead = false;
+    inputy=textArea.value;
+    console.log(inputy);
+    console.log(inputy.length);
+    
+    if (inputy == "" || inputy.length <= 0 || inputy.length > lengthCode){
+        console.log("INVALID")
+        ticketLabel.style.color = "red";
+        ticketLabel.innerHTML = "Ticket Votante Invalido";
+    }
+    else{
+        goAhead = true;
+    }
+
+    if(goAhead){
+        window.location.href = "huella.html";
+    }
+
+}
