63d8886e1dd61c0b43e45b80ef59bb74b16db014
davidesquer <der_1706@hotmail.com>
2021-04-20 18:09:53 -0600

    GeneraQr

---
+++ GeneraQr.py
@@ -0,0 +1,11 @@
+import pyqrcode
+
+ClaveElector = "CLaveDeElector"
+
+def generaQr(Clave):
+    qrCode = pyqrcode.create(Clave, error='L', version=27, mode='binary')
+    qrCode.png('code.png', scale=6, module_color=[0, 0, 0, 128], background=[0xff, 0xff, 0xcc])
+    qrCode.show()
+
+
+generaQr(ClaveElector)
